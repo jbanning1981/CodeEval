74689e8b9b94878e82d1afbdcf102aa518b51afd
Jim Banning <jbanning1981@hotmail.com>
2016-03-18 00:33:35 -0500

    Adding Panacea Solution
    Adding complete solution for Panacea- Truth or Lie
    
    https://www.codeeval.com/open_challenges/237/

---
+++ Panacea/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Panacea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = args[0];
+
+            using (StreamReader reader = File.OpenText(fileName))
+            {
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    if (String.IsNullOrWhiteSpace(line))
+                        continue;
+
+                    string[] inputs = line.Split('|');
+
+                    string[] hexValues = inputs[0].Split(' ');
+                    string[] binaryValues = inputs[1].Split(' ');
+
+                    int sumHex = 0;
+                    foreach(string s in hexValues)
+                    {
+                        if(!String.IsNullOrWhiteSpace(s))
+                        {
+                            sumHex += Convert.ToInt32(s, 16); //Base 16
+                        }
+                    }
+
+                    int sumBinary = 0;
+                    foreach(string s in binaryValues)
+                    {
+                        if (!String.IsNullOrWhiteSpace(s))
+                        {
+                            sumBinary += Convert.ToInt32(s, 2); //Base 2
+                        }
+                    }
+
+                    string result = (sumBinary >= sumHex).ToString();
+
+                    Console.WriteLine(result);
+                }
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
