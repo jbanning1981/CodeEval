68548b0dd2376c3b38634964fe41e8afcd4f45d1
Jim Banning <jbanning1981@gmail.com>
2016-03-19 03:33:49 -0500

    Completed
    Adding completed challenge for simple or trump
    
    https://www.codeeval.com/open_challenges/235/

--- Simple_Or_Trump/Program.cs
+++ Simple_Or_Trump/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,68 @@ namespace Simple_Or_Trump
     {
         static void Main(string[] args)
         {
+            string fileName = args[0];
+
+            var deck = GetDeck();
+
+            using (StreamReader reader = File.OpenText(fileName))
+            {
+
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    if (String.IsNullOrWhiteSpace(line))
+                        continue;
+
+                    string[] inputs = line.Split('|');
+
+                    string[] cards = inputs[0].Trim().Split(' ');
+                    string trump = inputs[1].Trim();
+                    string cardOne = cards[0];
+                    string cardTwo = cards[1];
+
+                    string winningCard = GetWinningCard(cardOne, cardTwo, trump, deck);
+
+                    Console.WriteLine(winningCard);
+
+                }
+            }
+
+            Console.ReadLine();
         }
+
+        public static string GetWinningCard(string cardOne, string cardTwo, string trumpSuit, string[] deck)
+        {
+            var isOneTrump = cardOne.Contains(trumpSuit);
+            var isTwoTrump = cardTwo.Contains(trumpSuit);
+
+            if (isOneTrump && !isTwoTrump) return cardOne;
+            if (isTwoTrump && !isOneTrump) return cardTwo;
+
+            int cardOneValue = Array.FindIndex(deck, s => s == cardOne.Substring(0, cardOne.Length -1).ToString());
+            int cardTwoValue = Array.FindIndex(deck, s => s == cardTwo.Substring(0, cardTwo.Length - 1).ToString());
+
+            string result = cardOne; //assume card one is the winner so we can eliminate an if statement
+            
+
+            if (cardTwoValue > cardOneValue)
+            {
+                result = cardTwo;
+            }
+            else if (cardOneValue == cardTwoValue)
+            {
+                result += " " + cardTwo;
+            }
+
+            return result;
+        }
+
+        public static string[] GetDeck()
+        {
+            var cardDeck = new string[] { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
+
+            return cardDeck;
+        }
+
     }
 }
